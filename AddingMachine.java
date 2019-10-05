f6ed85124c732c96cd04d04dc864d38eae20f39f
DougGeorge <38056037+DougGeorge@users.noreply.github.com>
2019-10-05 16:15:33 -0700

    Add files via upload
    Initial case for the Assignment 3

---
+++ AddingMachine.java
@@ -0,0 +1,41 @@
+/*
+ * Douglas George ID: 1213123351 assignment 1
+ * This program takes in values and adds or subtracts them together to get
+ * the total of the integers input.
+ */
+package cse360assign2;
+
+public class AddingMachine {
+
+	/*
+	 * The class AddingMachine includes a constructor, as well as 
+	 * methods to add, subtract and get the total value from the
+	 * integers input.
+	 */
+	
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () { // gets the total from the integers and returns it.
+		return 0;
+	}
+	
+	public void add (int value) { // adds an integer value from the total.
+		
+	}
+	
+	public void subtract (int value) { // subtracts an integer value from the total.
+		
+	}
+		
+	public String toString () { // changes the integers to a string to be printed.
+		return "";
+	}
+
+	public void clear() { // clears the values to start over.
+	
+	}
+}
